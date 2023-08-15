2bd4d128503b69615b679c5edf66ba2c91628850
emil.asadov88 <emil.asadov88@gmail.com>
2023-08-15 16:41:00 +0400

    Single Responsibility Principle

---
+++ SingleResponsibilityPrinciple/Model/ClassCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.Model
+{
+    internal class ClassCustomer
+    {
+        public DataTable GetCustomers()
+        {
+            return new DataTable();
+        }
+    }
+}
---
+++ SingleResponsibilityPrinciple/Model/ClassDbConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.Model
+{
+    internal class ClassDbConfig
+    {
+        public string GetConnectionString()
+        {
+            return string.Empty;
+        }
+        public bool CreateConnection()
+        {
+            return true;
+        }
+        public bool CloseConection()
+        {
+            return true;
+        }
+    }
+}
